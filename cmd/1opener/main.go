1d476b1f363d6f1414486d765333a328a9072d06
Arvind Subramanian <asubramanian@stratussolutions.com>
2019-12-10 06:39:12 -0500

    initial commit

---
+++ cmd/1opener/main.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+If we list all the natural numbers below 30 that are multiples of 7 or 9, we get 7, 9, 14, 18, 21, 27, and 28.
+The sum of these multiples is 124.
+Find the sum of all the multiples of 7 or 9 below BUT NOT INCLUDING 30,000.
+*/
+func main() {
+
+}
---
+++ cmd/2struct/main.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+Visit https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400 and model a struct off of this
+Use fmt.Printf with the %+v option to print it out to screen
+*/
+func main() {
+
+}
