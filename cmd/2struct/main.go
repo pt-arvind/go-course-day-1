84e6f331ec8c119d721fa4b0a989a9fbc475717a
Arvind Subramanian <asubramanian@stratussolutions.com>
2019-12-10 07:20:40 -0500

    2struct

--- cmd/2struct/main.go
+++ cmd/2struct/main.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 Visit https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400 and model a struct off of this
 Use fmt.Printf with the %+v option to print it out to screen
 */
-func main() {
 
+// SunInfo defines sunrise-sunset.org API response
+type SunInfo struct {
+	Sunrise                   time.Time
+	Sunset                    time.Time
+	SolarNoon                 time.Time
+	DayLength                 time.Duration
+	CivilTwilightBegin        time.Time
+	CivilTwilightEnd          time.Time
+	NauticalTwilightBegin     time.Time
+	NauticalTwilightEnd       time.Time
+	AstronomicalTwilightBegin time.Time
+	AstronomicalTwilightEnd   time.Time
+}
+
+func main() {
+	fmt.Printf("%+v\n", SunInfo{})
 }
--- cmd/3json/main.go
+++ cmd/3json/main.go
@@ -5,7 +5,23 @@ package main
 	print the struct to the screeen
 	Copy the struct to an 'internal' folder outside the 'cmd' folder to preserve golang conventions
 */
-var jsonString = `{"results":{"sunrise":"7:19:21 AM","sunset":"5:01:46 PM","solar_noon":"12:10:33 PM","day_length":"09:42:25","civil_twilight_begin":"6:50:42 AM","civil_twilight_end":"5:30:25 PM","nautical_twilight_begin":"6:18:24 AM","nautical_twilight_end":"6:02:43 PM","astronomical_twilight_begin":"5:46:58 AM","astronomical_twilight_end":"6:34:09 PM"},"status":"OK"}`
+var jsonString = `
+{
+	"results": {
+		"sunrise": "7:19:21 AM",
+		"sunset": "5:01:46 PM",
+		"solar_noon": "12:10:33 PM",
+		"day_length": "09:42:25",
+		"civil_twilight_begin": "6:50:42 AM",
+		"civil_twilight_end": "5:30:25 PM",
+		"nautical_twilight_begin": "6:18:24 AM",
+		"nautical_twilight_end": "6:02:43 PM",
+		"astronomical_twilight_begin": "5:46:58 AM",
+		"astronomical_twilight_end": "6:34:09 PM"
+	},
+	"status": "OK"
+}
+`
 
 func main() {
 
